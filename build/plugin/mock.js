b82a1b65cb7291eed9634afe7b9a0fedf67c98b9
Alan <sz9751210@gmail.com>
2024-02-21 15:15:06 +0800

    修正 import error

--- build/plugin/mock.js
+++ build/plugin/mock.js
@@ -1,5 +1,5 @@
 // 从vite-plugin-mock包中导入viteMockServer函数
-import { viteMockServer } from 'vite-plugin-mock'
+import { viteMockServe } from 'vite-plugin-mock'
 
 /**
  * 配置并返回mock插件的实例
@@ -12,7 +12,7 @@ import { viteMockServer } from 'vite-plugin-mock'
  */
 export function configMockPlugin(isBuild) {
   // 调用viteMockServer函数，配置mock插件
-  return viteMockServer({
+  return viteMockServe({
     // 指定mock文件所在目录
     mockPath: 'mock/modules',
     // 控制在非构建模式（即开发模式）下是否启用mock
